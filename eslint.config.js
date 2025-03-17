6c9636fdbcb3d31e69c1fe96af8fa39c1095a90a
Alexander Novitsky <novitsky.413x@gmail.com>
2025-03-17 09:56:49 +0300

    changed eslint config, added debug launcher

--- eslint.config.js
+++ eslint.config.js
@@ -2,13 +2,13 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import pluginReact from 'eslint-plugin-react';
 import pluginReactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
     { files: ['**/*.{js,mjs,cjs,jsx}'] },
     { languageOptions: { globals: globals.browser } },
-    pluginJs.configs.recommended,
-    pluginReact.configs.flat.recommended,
     {
         rules: {
             'react/react-in-jsx-scope': 'off',
@@ -16,6 +16,10 @@ export default [
             'no-unused-vars': 'warn',
         },
     },
+    pluginJs.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    reactRefresh.configs.recommended,
+    eslintConfigPrettier,
     {
         plugins: {
             'react-hooks': pluginReactHooks,
